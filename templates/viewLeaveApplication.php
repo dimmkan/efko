f41ed0cb34facd70bef9add2c8153dbb81ecd430
Dmitry <dimmkan@mail.ru>
2019-12-14 16:45:20 +0300

    Добавил таблицу для вывода списка заявок

--- templates/viewLeaveApplication.php
+++ templates/viewLeaveApplication.php
@@ -17,15 +17,37 @@
         <td colspan="3"  >
     <tr>
         <td valign="top" colspan="4">
-            <table width="90%" border="0" cellspacing="25">
+            <table width="200%" border="0" cellspacing="25">
                 <tr>
                     <td width="8%" valign="top"><img src="../includes/styles/styleListLA/single_pixel.gif" width="140" height="1"><br>
                         <p>Меню<br>
                             <a  class="menu" href="../index.php?action=listLA">К списку</a><br>
                             <a  class="menu" href="../index.php?action=logout">Выйти</a> </p>
                     </td>
-                    <td width="92%">
-
+                    <td align="center" width="92%">
+                        <table class="table">
+                            <tr class="tr">
+                                <th class="th">Номер заявки</th>
+                                <th class="th">ФИО Сотрудника</th>
+                                <th class="th">Начало отпуска</th>
+                                <th class="th">Конец отпуска</th>
+                                <th class="th">Описание</th>
+                            </tr>
+                            <tr class="tr">
+                                <td class="td">Microsoft</td>
+                                <td class="td">20.3</td>
+                                <td class="td">30.5</td>
+                                <td class="td">23.5</td>
+                                <td class="td">40.3</td>
+                            </tr>
+                            <tr class="tr">
+                                <td class="td">Google</td>
+                                <td class="td">50.2</td>
+                                <td class="td">40.63</td>
+                                <td class="td">45.23</td>
+                                <td class="td">39.3</td>
+                            </tr>
+                        </table>
                     </td>
                 </tr>
             </table>
